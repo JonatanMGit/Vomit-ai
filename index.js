314b71275f15921d0dd317ce230a205e2f0f3e6a
Jonatan_M <60609358+JonatanMGit@users.noreply.github.com>
2021-06-08 21:20:06 +0200

    Fix Bot

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const discord = require('discord.js');
 const config = require('./settings.json');
 const client = new discord.Client();
 const disbut = require('discord-buttons');
+const Tenor = require("tenorjs").client({
+	"Key": config.tenorkey, // https://tenor.com/developer/keyregistration
+	"Filter": "off", // "off", "low", "medium", "high", not case sensitive
+	"Locale": "de_DE", // Your locale here, case-sensitivity depends on input
+	"MediaFilter": "minimal", // either minimal or basic, not case sensitive
+	"DateFormat": "D/MM/YYYY - H:mm:ss A" // Change this accordingly
+});
 
 if (config.token == "Sus") {
 	console.log("Please enter you Discord Token in the settings file")
@@ -10,4 +17,14 @@ if (config.token == "Sus") {
 	client.login(config.token);
 }
 
-client.channels.cache.get("851892495482355753").send("vomit")
+
+
+
+client.on('message', (message) => {
+	Tenor.Search.Query(message.content, "1").then(Results => {
+		if (message.author.bot) return;
+		Results.forEach(Post => {
+			client.channels.cache.get("851892495482355753").send(`${Post.url}`);
+		});
+	}).catch(console.error);
+})
