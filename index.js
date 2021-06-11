dad0c9cbf4a88ec6ed7792eab31a3651c899991d
Jonatan_M <60609358+JonatanMGit@users.noreply.github.com>
2021-06-11 20:20:03 +0200

    Send Vomit Message every full hour

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const discord = require('discord.js');
 const config = require('./settings.json');
 const client = new discord.Client();
 const disbut = require('discord-buttons');
+var schedule = require('node-schedule');
 const Tenor = require("tenorjs").client({
 	"Key": config.tenorkey, // https://tenor.com/developer/keyregistration
 	"Filter": "off", // "off", "low", "medium", "high", not case sensitive
@@ -31,7 +32,16 @@ client.on('message', (message) => {
 	howmany = Math.min(Math.max(parseInt(howmany), 1), 5);
 	Tenor.Search.Query(searchquery, howmany).then(Results => {
 		Results.forEach(Post => {
-			client.channels.cache.get("851892495482355753").send(`${Post.url}`);
+			client.channels.cache.get("851892495482355753").send(Post.url);
 		});
 	}).catch(console.error);
 })
+
+schedule.scheduleJob('0 * * * *', function () {
+	Tenor.Search.Random("vomit puke", "1").then(Results => {
+		Results.forEach(Post => {
+			client.channels.cache.get("851892495482355753").send("Es ist zeit");
+			client.channels.cache.get("851892495482355753").send(Post.url);
+		});
+	}).catch(console.error);
+});
