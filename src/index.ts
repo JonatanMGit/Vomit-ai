a35ad5ce29b0994e7713d5e6ae6e2ba6bfb474cf
Jonatan_M <60609358+JonatanMGit@users.noreply.github.com>
2021-08-10 19:39:18 +0200

    Add Linting and convert every file to LF

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,88 @@
-import { Client, Message, TextChannel } from "discord.js";
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { Client, TextChannel } from "discord.js";
 const config = require("./settings.js");
 const client = new Client(config.token);
-const schedule = require("node-schedule");
+import schedule = require("node-schedule");
 
 const Tenor = require("tenorjs").client({
-  Key: config.TenorToken, // https://tenor.com/developer/keyregistration
-  Filter: "off", // "off", "low", "medium", "high", not case sensitive
-  Locale: "de_DE", // Your locale here, case-sensitivity depends on input
-  MediaFilter: "minimal", // either minimal or basic, not case sensitive
-  DateFormat: "D/MM/YYYY - H:mm:ss A", // Change this accordingly
+	Key: config.TenorToken, // https://tenor.com/developer/keyregistration
+	Filter: "off", // "off", "low", "medium", "high", not case sensitive
+	Locale: "de_DE", // Your locale here, case-sensitivity depends on input
+	MediaFilter: "minimal", // either minimal or basic, not case sensitive
+	DateFormat: "D/MM/YYYY - H:mm:ss A", // Change this accordingly
 });
 
 client.login(config.DisToken);
 
 client.on("ready", () => {
-  client.guilds.cache.forEach((guild) => {
-    console.log(`${guild.name} | ${guild.id}`);
-  });
+	client.guilds.cache.forEach((guild) => {
+		console.log(`${guild.name} | ${guild.id}`);
+	});
 });
 
 client.on("message", (message) => {
-  if (message.author.bot) return;
-  if (message.content.toLowerCase().includes("sex")) {
-    message.reply("Bitte kein sex in der Nachricht beinhalten!");
-    return;
-  }
+	if (message.author.bot) return;
+	if (message.content.toLowerCase().includes("sex")) {
+		message.reply("Bitte kein sex in der Nachricht beinhalten!");
+		return;
+	}
 
-  if (message.content.includes("howmuch:")) {
-    var howmany: number = Math.min(
-      Math.max(parseInt(message.content.replace(/^\D+/g, "")), 1),
-      5
-    );
-  }
-  let searchquery = message.content.replace(/howmuch.*$/, "");
+	let howmany;
+	if (message.content.includes("howmuch:")) {
+		howmany = Math.min(
+			Math.max(parseInt(message.content.replace(/^\D+/g, "")), 1),
+			5
+		);
+	}
+	const searchquery = message.content.replace(/howmuch.*$/, "");
 
-  Tenor.Search.Random(searchquery, howmany)
-    .then((Results) => {
-      Results.forEach((Post) => {
-        (client.channels.cache.get("851892495482355753") as TextChannel)
-          .send(Post.url)
-          .catch(console.error);
-        if (config.Verbose)
-          console.log(`Query ${searchquery} responded with ${Post.url}`);
-      });
-    })
-    .catch(console.error);
+	Tenor.Search.Random(searchquery, howmany)
+		.then((Results) => {
+			Results.forEach((Post) => {
+				(client.channels.cache.get("851892495482355753") as TextChannel)
+					.send(Post.url)
+					.catch(console.error);
+				if (config.Verbose)
+					console.log(`Query ${searchquery} responded with ${Post.url}`);
+			});
+		})
+		.catch(console.error);
 });
 
 schedule.scheduleJob("0 * * * *", function () {
-  Tenor.Search.Random("vomit puke", "2")
-    .then((Results) => {
-      Results.forEach((Post) => {
-        (client.channels.cache.get("851892495482355753") as TextChannel)
-          .send("Es ist zeit")
-          .catch(console.error);
-      });
-    })
-    .catch(console.error);
+	(client.channels.cache.get("851892495482355753") as TextChannel)
+		.send("Es ist zeit")
+		.catch(console.error);
+	Tenor.Search.Random("vomit puke", "2")
+		.then((Results) => {
+			Results.forEach((Post) => {
+				(client.channels.cache.get("851892495482355753") as TextChannel)
+					.send(Post.url)
+					.catch(console.error);
+				if (config.Verbose) console.log(`Full Hour responded with ${Post.url}`);
+			});
+		})
+		.catch(console.error);
 });
 
 schedule.scheduleJob("30 * * * *", function () {
-  Tenor.Search.Random("30", "1")
-    .then((Results) => {
-      Results.forEach((Post) => {
-        (client.channels.cache.get("851892495482355753") as TextChannel)
-          .send("30 Minuten bis zur zeit") 
-          .catch(console.error);
-        if (config.Verbose) console.log(`Half Hour responded with ${Post.url}`);
-      });
-    })
-    .catch(console.error);
+	(client.channels.cache.get("851892495482355753") as TextChannel)
+		.send("30 Minuten bis zur zeit")
+		.catch(console.error);
+	Tenor.Search.Random("30", "1")
+		.then((Results) => {
+			Results.forEach((Post) => {
+				(client.channels.cache.get("851892495482355753") as TextChannel)
+					.send(Post.url)
+					.catch(console.error);
+				if (config.Verbose) console.log(`Full Hour responded with ${Post.url}`);
+				if (config.Verbose) console.log(`Half Hour responded with ${Post.url}`);
+			});
+		})
+		.catch(console.error);
 });
 
 process.on("uncaughtException", function (err) {
-  console.error(err);
-  console.log("Probably EAI_AGAIN error");
+	console.error(err);
+	console.log("Probably EAI_AGAIN error");
 });
