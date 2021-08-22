99e24c8196227adcc80da6acb1a951b7c0a5a30d
Jonatan_M <60609358+JonatanMGit@users.noreply.github.com>
2021-08-22 18:58:47 +0200

    More descriptive Ping Command description

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,7 +3,7 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("ping")
-		.setDescription("Replies with Pong!"),
+		.setDescription("Replies with Pong! and the Roundtrip latency"),
 	async execute(interaction) {
 		const sent = await interaction.reply({ content: "Pinging...", fetchReply: true });
 		interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
