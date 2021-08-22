3a2ef602baf9a8e0fa63da99d0ed7e3bc630df94
Jonatan_M <60609358+JonatanMGit@users.noreply.github.com>
2021-08-22 15:02:37 +0200

    Make config error messages more clear

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 
 module.exports = {
-	DisToken: process.env.DisToken || "" || console.error("Enter Your Discord Token in the Settings file!"),
-	TenorToken: process.env.TenToken || "" || console.error("Enter Your Tenor Token in the Settings file!"),
+	DisToken: process.env.DisToken || "" || console.error("Enter Your Discord Token in the Settings file or create an Enviroment Variable called 'DisToken' !"),
+	TenorToken: process.env.TenToken || "" || console.error("Enter Your Tenor Token in the Settings file or create an Enviroment Variable called 'TenorToken' !"),
 	Verbose: process.env.VerboseLogging || false,
 	sendgifs: process.env.SendGifs || false
 };
