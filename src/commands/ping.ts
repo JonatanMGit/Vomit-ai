2ff074c5cf067b51c644ac9ed2eea4d6584c289c
Jonatan_M <60609358+JonatanMGit@users.noreply.github.com>
2021-09-05 23:01:06 +0200

    Add Typings to commands

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,11 +1,12 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction, Message } from "discord.js";
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("ping")
 		.setDescription("Replies with the Roundtrip Time!"),
-	execute: async (interaction) => {
+	execute: async (interaction: CommandInteraction) => {
 		const sent = await interaction.reply({ content: "Pinging...", fetchReply: true });
-		interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+		interaction.editReply(`Roundtrip latency: ${(sent as Message).createdTimestamp - interaction.createdTimestamp}ms`);
 	},
 };
