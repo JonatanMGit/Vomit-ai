0935ba56800805484c5c59ea32458e5326257f6a
Jonatan_M <60609358+JonatanMGit@users.noreply.github.com>
2021-09-05 22:33:37 +0200

    Move to fat arrow function

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,8 +3,8 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("ping")
-		.setDescription("Replies with Pong! and the Roundtrip latency"),
-	async execute(interaction) {
+		.setDescription("Replies with the Roundtrip Time!"),
+	execute: async (interaction) => {
 		const sent = await interaction.reply({ content: "Pinging...", fetchReply: true });
 		interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
 	},
